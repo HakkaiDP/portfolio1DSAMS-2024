40a5bf6e91423b6f33fa9c74dc244115e14f0f4f
AntonioBSN <166918368+HakkaiDP@users.noreply.github.com>
2025-09-04 09:51:25 -0300

    .

--- Atividades extracurriculares/Python/Primeiros Projetos/Nascimento.py
+++
@@ -1,4 +0,0 @@
-Dia = input('Qual o dia do seu nascimento ')
-Mes = input('Qual o mês do seu nascimento ')
-Ano = input('Qual o ano do seu nascimento ')
-print(f'Você nasceu no dia {Dia} de {Mes} de {Ano}, correto?')
